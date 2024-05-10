1bbbcddb02255550b7424057e8fa805ea47b6e1b
Your Name <you@example.com>
2024-05-10 14:10:42 +0700

    edit ex-1,ex-2ex-3

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,15 @@
 // Exercise #1: Food Order
 
 // Start coding here
+
+const FoodOrder = {
+  orderNumber: "A0234",
+  address: "Bangkok",
+  paymentType: "Cash",
+  totalPrice: 4500,
+  restaurantName: "MK",
+};
+
+(FoodOrder.totalPrice = 500),
+  (FoodOrder.paymentType = "Credit Card"),
+  console.log(FoodOrder);

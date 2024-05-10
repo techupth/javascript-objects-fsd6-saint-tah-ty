d657a80944e057bff71bc93a83b975d073633bf2
Wongsathon Chuijit <wongsathon@Wongsathons-MacBook-Air.local>
2024-05-10 14:38:23 +0700

    feat:object

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,15 @@
 // Exercise #1: Food Order
 
 // Start coding here
+const foodOrder = {
+  orderumumber: "A0234",
+  ddress: "Bangkok",
+  paymentType: "cash",
+  totalPrice: 4500,
+  restaurantName: "MK",
+};
+
+foodOrder.totalPrice = 5000;
+foodOrder.paymentType = "Credit Card";
+
+console.log(foodOrder);
